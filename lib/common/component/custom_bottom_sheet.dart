e7ffabce4906ce87b7ef2ffbc7ebca6c5d8cf378
yeongwoo <jyy0223@naver.com>
2023-08-22 20:43:59 +0900

    [add] components item

---
+++ lib/common/component/custom_bottom_sheet.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:true_counter/common/const/button_style.dart';
+import 'package:true_counter/common/const/colors.dart';
+import 'package:true_counter/common/const/text_style.dart';
+
+class CustomBottomSheet extends StatelessWidget {
+  const CustomBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: Container(
+              color: EMPTY_COLOR,
+            ),
+          ),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            color: BACKGROUND_COLOR,
+            borderRadius: BorderRadius.vertical(
+              top: Radius.circular(16.0),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 24.0,
+              horizontal: 16.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('asdfasdf'),
+                ),
+                const SizedBox(height: 12.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: secondButtonStyle,
+                  child: Text(
+                    'asdfasdf',
+                    style: bodyMediumTextStyle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/common/component/custom_text_form_field.dart
+++ lib/common/component/custom_text_form_field.dart
@@ -8,58 +8,64 @@ enum KeyboardType {
 }
 
 class CustomTextFormField extends StatelessWidget {
-  final String title;
+  final String? title;
   final GestureTapCallback? onPressed;
   final KeyboardType keyboardType;
   final ValueChanged<String>? onChanged;
   final bool obscureText;
   final String? hintText;
   final int? maxLength;
+  final bool? realOnly;
 
   const CustomTextFormField({
     Key? key,
-    required this.title,
+    this.title,
     this.onPressed,
     this.keyboardType = KeyboardType.everything,
     this.onChanged,
     this.obscureText = false,
     this.hintText,
     this.maxLength,
+    this.realOnly,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 4.0),
-          child: SizedBox(
-            width: 80.0,
+        if (title != null)
+          SizedBox(
+            width: 120.0,
             child: Text(
-              title,
+              title!,
               style: bodyMediumTextStyle,
             ),
           ),
-        ),
-        const SizedBox(width: 4.0),
+        if (title != null) const SizedBox(width: 16.0),
         Expanded(
-          child: TextFormField(
-            obscureText: obscureText,
-            onChanged: onChanged,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
+          child: SizedBox(
+            height: 48.0,
+            child: TextFormField(
+              obscureText: obscureText,
+              onChanged: onChanged,
+              decoration: InputDecoration(
+                // contentPadding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                hintText: hintText,
               ),
-              hintText: hintText,
+              maxLength: maxLength,
+              keyboardType: keyboardType == KeyboardType.number
+                  ? TextInputType.number
+                  : TextInputType.multiline,
+              inputFormatters: keyboardType == KeyboardType.number
+                  ? [FilteringTextInputFormatter.digitsOnly]
+                  : [],
+              readOnly: realOnly ?? false,
             ),
-            maxLength: maxLength,
-            keyboardType: keyboardType == KeyboardType.number
-                ? TextInputType.number
-                : TextInputType.multiline,
-            inputFormatters: keyboardType == KeyboardType.number
-                ? [FilteringTextInputFormatter.digitsOnly]
-                : [],
           ),
         ),
       ],

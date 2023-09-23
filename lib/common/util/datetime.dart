14c78617c46dca9e0a9aec051b09b5a480d7d021
yeongwoo <jyy0223@naver.com>
2023-09-23 18:06:42 +0900

    [add] convert datetime 추가

--- lib/common/util/datetime.dart
+++ lib/common/util/datetime.dart
@@ -3,3 +3,9 @@ String convertDateTimeToDateString({
 }) {
   return '${datetime.year}-${datetime.month.toString().padLeft(2, '0')}-${datetime.day.toString().padLeft(2, '0')}';
 }
+
+String convertDateTimeToDateTimeString({
+  required DateTime datetime,
+}) {
+  return '${datetime.year}-${datetime.month.toString().padLeft(2, '0')}-${datetime.day.toString().padLeft(2, '0')} ${datetime.hour.toString().padLeft(2, '0')}시';
+}

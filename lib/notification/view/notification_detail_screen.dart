290868470bceea9e0f182567dfd554dc7703ec29
yeongwoo <jyy0223@naver.com>
2023-12-09 16:09:05 +0900

    [add] 공지사항 uri launch 기능 구현

--- lib/notification/view/notification_detail_screen.dart
+++ lib/notification/view/notification_detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:true_counter/common/const/colors.dart';
 import 'package:true_counter/common/const/text_style.dart';
 import 'package:true_counter/common/layout/default_appbar.dart';
@@ -16,6 +18,8 @@ class NotificationDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> contents = notificationModel.content.split('\n').toList();
+
     return DefaultLayout(
       appbar: const DefaultAppBar(
         title: '공지 상세보기',
@@ -41,9 +45,32 @@ class NotificationDetailScreen extends StatelessWidget {
               const SizedBox(height: 16.0),
               const Divider(),
               const SizedBox(height: 16.0),
-              SelectableText(
-                notificationModel.content,
-                style: MyTextStyle.bodyMedium,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: List<Widget>.generate(
+                  contents.length,
+                  (index) => contents[index].contains('https://')
+                      ? RichText(
+                          text: TextSpan(
+                            text: contents[index],
+                            style: MyTextStyle.bodyMedium.copyWith(
+                              color: Colors.blue,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                launchBrowserTab(
+                                  Uri.parse(
+                                    contents[index],
+                                  ),
+                                );
+                              },
+                          ),
+                        )
+                      : SelectableText(
+                          contents[index],
+                          style: MyTextStyle.bodyMedium,
+                        ),
+                ),
               ),
             ],
           ),
